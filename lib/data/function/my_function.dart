8dcfbd8a83a897053e69fe4f3d42293cc329a02d
Sandra PC <119578353+Sandrapc1@users.noreply.github.com>
2025-04-21 12:28:25 +0530

    Update my_function.dart
    change codes

--- lib/data/function/my_function.dart
+++ lib/data/function/my_function.dart
@@ -1,31 +1,3 @@
-// import 'package:flutter/material.dart';
-// import 'package:get/get.dart';
-
-// class MyFunctions {
-//   void updateAnimationDuration(Duration duration, AnimationController controller) {
-//     controller.duration = duration;
-//     if (controller.isAnimating) {
-//       controller
-//         ..stop()
-//         ..forward();
-//     }
-//   }
-
-//   void updateProgress(
-//       int totalItems, int itemsInLine, RxDouble progress, RxBool isReversed) {
-//     double newProgress = itemsInLine / totalItems;
-//     progress.value = isReversed.value ? 1.0 - newProgress : newProgress;
-//   }
-
-//   void toggleReverseAnimation(
-//       AnimationController controller, RxBool isReversed) {
-//     if (controller.isAnimating) {
-//       controller.stop();
-//     }
-//     controller.value = isReversed.value ? 1.0 - controller.value : controller.value;
-//     controller.forward();
-//   }
-// }
 
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
